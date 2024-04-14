31e956112c7fe66c71e1d94983ee97b08ae06744
Marquis Abah <marquis.abah@shuttlers.ng>
2024-04-14 09:08:56 +0100

    removed poilygon

--- pages/index.vue
+++ pages/index.vue
@@ -183,13 +183,13 @@
 
       <div class="relative isolate overflow-hidden pt-14">
         <img src="@/assets/img/bg.png" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
-        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        <!-- <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true">
           <div
             class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
           </div>
-        </div>
+        </div> -->
         <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <!-- <div class="hidden sm:mb-8 sm:flex sm:justify-center">
             <div
@@ -213,14 +213,14 @@
             </div>
           </div>
         </div>
-        <div
+        <!-- <div
           class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true">
           <div
             class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
     <div class="flex justify-center items-center lg:gap-x-32 py-3">
@@ -1430,13 +1430,13 @@
           <div class="space-y-4">
             <h2 class="text-sm font-light leading-8 text-gray-500">Partnerships:</h2>
             <div class="flex items-center gap-x-8">
-              <img class="col-span-2 max-h-12 w-20 object-contain object-left lg:col-span-1"
+              <img class="col-span-2 max-h-12 w-16 object-contain object-left lg:col-span-1"
                 src="https://www.netguru.com/hubfs/_N23/assets/logos/microsoft.svg" alt="Statamic">
-              <img class="col-span-2 max-h-12 w-20 object-contain object-left lg:col-span-1" src="https://www.netguru.com/hubfs/_N23/assets/logos/aws-partner.svg
+              <img class="col-span-2 max-h-12 w-16 object-contain object-left lg:col-span-1" src="https://www.netguru.com/hubfs/_N23/assets/logos/aws-partner.svg
                 " alt="Statamic">
-              <img class="col-span-2 max-h-12 w-20 object-contain object-left lg:col-span-1" src="https://www.netguru.com/hubfs/_N23/assets/logos/mendix.svg
+              <img class="col-span-2 max-h-12 w-16 object-contain object-left lg:col-span-1" src="https://www.netguru.com/hubfs/_N23/assets/logos/mendix.svg
                 " alt="Statamic">
-              <img class="col-span-2 max-h-12 w-20 object-contain object-left lg:col-span-1" src="	https://www.netguru.com/hubfs/_N23/assets/logos/google-cloud-partner.svg
+              <img class="col-span-2 max-h-12 w-16 object-contain object-left lg:col-span-1" src="	https://www.netguru.com/hubfs/_N23/assets/logos/google-cloud-partner.svg
                 " alt="Statamic">
             </div>
           </div>
